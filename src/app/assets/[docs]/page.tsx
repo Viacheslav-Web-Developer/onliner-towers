725a23caa201af851c21ae0a41e089ecc966e7d4
Viacheslav Parubtsev <slavaparybcev2003@gmail.com>
2024-02-23 16:56:29 +0300

    url test pdf viewer

--- src/app/assets/[docs]/page.tsx
+++ src/app/assets/[docs]/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 import React from 'react';
 import {useParams} from "next/navigation";
-import PdfViewer from "@/components/PdfViewer/PdfViewer";
+// import PdfViewer from "@/components/PdfViewer/PdfViewer";
 
 const Page = () => {
     const params: { docs?: string } = useParams()
 
-    console.log(params);
+    console.log(params)
 
-    return params.docs ? <PdfViewer pdfName={params.docs}/> : <div>none</div>;
+    return (
+        <div>
+            awdawd
+        </div>
+    )
 
+    // return params.docs ? <PdfViewer pdfName={params.docs}/> : <div>none</div>
 };
 
 export default Page;
